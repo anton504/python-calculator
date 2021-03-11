4e36638ee7215fcad1ffc3bbf2247153e65436a3
anton504 <talizin.anton@gmail.com>
2021-03-11 19:12:31 +0200

    Create calculator.py
    calculator

---
+++ calculator.py
@@ -0,0 +1,15 @@
+#my first programme on python
+
+#inicialization of numbers
+num1 = float(input('First number: '))
+num2 = float(input('Second number: '))
+
+act = str(input("What we are gonna do: "))
+
+#functions
+
+#conditions
+if act == '+':
+	res = num1 + num2
+	print("Here is result: ", res)
+	
